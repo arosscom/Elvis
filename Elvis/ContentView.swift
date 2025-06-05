cab03f493e2ad8a3a3d25aa71f032dd54f4a4e84
A. Ross Morton <arossco@ntlworld.com>
2025-06-05 10:32:05 +0100

    Initial commit of Elvis project

--- Elvis/ContentView.swift
+++ Elvis/ContentView.swift
@@ -8,12 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var message = "Love"
+   
+    @State private var image = Image(systemName: "heart")
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            VStack() {
+                HStack {
+                    Text("  What's So Funny")
+                    Spacer()
+                }
+                HStack {
+                    Text("  'Bout")
+                    Spacer()
+                }
+            }
+            .font(.largeTitle)
+            .fontWeight(.heavy)
+            .foregroundStyle(.purple)
+                
+            Spacer()
+ 
+            image
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+            
+            Spacer()
+        
+            Text(message)
+                .font(.system(size: 48))
+                .fontWeight(.heavy)
+                .foregroundStyle(.purple)
+            
+            Spacer()
+
+            
+            HStack {
+                Button("Peace") {
+                    message = "Peace"
+                    image = Image(systemName: "peacesign")
+                }
+                Button("Love") {
+                    message = "Love"
+                    image = Image(systemName: "heart")
+                }
+                Button("Understanding") {
+                    message = "Understanding"
+                    image = Image(systemName: "lightbulb")
+                }
+
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.purple)
+            
+            
         }
         .padding()
     }
