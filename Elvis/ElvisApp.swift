89472ddfa5f114897a9cf84536e2d171077cef2a
A. Ross Morton <arossco@ntlworld.com>
2025-06-05 09:49:15 +0100

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Ross Morton on 05/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
